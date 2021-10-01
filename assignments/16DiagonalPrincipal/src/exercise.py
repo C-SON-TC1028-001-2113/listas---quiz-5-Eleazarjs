3f86d71ca95e3370e53133ca004d30bad25b37e7
Eleazarjs <A01252485@itesm.mx>
2021-10-01 17:53:24 +0000

    16DiagonalPrincipal

--- assignments/16DiagonalPrincipal/src/exercise.py
+++ assignments/16DiagonalPrincipal/src/exercise.py
@@ -1,6 +1,21 @@
 
 def main():
-  
-
+    u = int(input())
+    o = int(input())
+    p = u*o
+    lista1=[]
+    lista2=[]
+    index=0
+    if u==o:
+        for i in range (p):
+            l=int(input())
+            lista1.append(l)
+        while index <p:
+            e=lista1[index]
+            lista2.append(e)
+            index=index+(u+1)
+        print(lista2)
+    else:
+        print('La matriz no es cuadrada')
 if __name__=='__main__':
     main()
